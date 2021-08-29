52f562d275c5e8415e486c7e529094d534167779
Szymon Kluba <szymonkluba@gmail.com>
2021-08-29 22:24:16 +0200

    Changes serializers classes to correct declaration:D

--- price_scraper/apps/products/serializers.py
+++ price_scraper/apps/products/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 
 from .models import Product, Category
+from ..price_lookup.serializers import PriceSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    model = Product
-    fields = ["name", "popularity", "category", "current_prices"]
+
+    class Meta:
+        model = Product
+        fields = ["name", "popularity", "category", "current_prices"]
+
+        current_prices = PriceSerializer(many=True, read_only=True)
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    model = Category
-    fields = ["name"]
+
+    class Meta:
+        model = Category
+        fields = ["name"]
