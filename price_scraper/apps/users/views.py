bf7e7bc3b7d9a42f5f1fb864c8c8eb61546c0ef5
Szymon Kluba <szymonkluba@gmail.com>
2021-08-29 11:17:19 +0200

    Adds permission class for users app views.

--- price_scraper/apps/users/views.py
+++ price_scraper/apps/users/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, mixins, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from ..products.models import Product
 from ..products.serializers import ProductSerializer
-from .models import CustomUser
 
 
 class ListUpdateDeleteViewSet(mixins.ListModelMixin,
@@ -13,16 +14,28 @@ class ListUpdateDeleteViewSet(mixins.ListModelMixin,
 
 
 class FavouritesViewSet(ListUpdateDeleteViewSet):
-
-    queryset = CustomUser.favourites.objects.all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def list(self, request, *args, **kwargs):
+        user = request.user
+        products = user.favourites.objects.all()
+
+        if products is not None:
+            serializer = self.get_serializer(products, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     def destroy(self, request, *args, **kwargs):
         product = self.get_object()
         request.user.favourites.objects.remove(product)
+
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def update(self, request, *args, **kwargs):
         product = self.get_object()
         request.user.favourites.objects.add(product)
+
         return Response(status=status.HTTP_200_OK)
