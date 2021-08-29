fb9329509b1f421e699823102d43baff92cb155f
Szymon Kluba <szymonkluba@gmail.com>
2021-08-29 10:43:07 +0200

    Adds views for user's favourites.

--- price_scraper/apps/users/views.py
+++ price_scraper/apps/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
 
-# Create your views here.
+from ..products.serializers import ProductSerializer
+from .models import CustomUser
+
+
+class ListUpdateDeleteViewSet(mixins.ListModelMixin,
+                              mixins.UpdateModelMixin,
+                              mixins.DestroyModelMixin,
+                              viewsets.GenericViewSet):
+    pass
+
+
+class FavouritesViewSet(ListUpdateDeleteViewSet):
+
+    queryset = CustomUser.favourites.objects.all()
+    serializer_class = ProductSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        product = self.get_object()
+        request.user.favourites.objects.remove(product)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def update(self, request, *args, **kwargs):
+        product = self.get_object()
+        request.user.favourites.objects.add(product)
+        return Response(status=status.HTTP_200_OK)
