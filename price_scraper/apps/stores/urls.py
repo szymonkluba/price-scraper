891de9d3bb6aa0b70fdc61b88ab9aef383076b63
Szymon Kluba <szymonkluba@gmail.com>
2021-08-29 12:01:20 +0200

    Changes imports.

--- price_scraper/apps/stores/urls.py
+++ price_scraper/apps/stores/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from price_scraper.apps.stores.views import StoreViewSet
+from .views import StoreViewSet
 
 router = DefaultRouter()
 router.register(r'stores', StoreViewSet)
