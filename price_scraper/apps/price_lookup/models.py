ae6e1f47de1ebc55678612a64ffc681d8e2c0c46
Szymon Kluba <szymonkluba@gmail.com>
2021-08-30 20:25:18 +0200

    Updates in price_lookup and stores models.

--- price_scraper/apps/price_lookup/models.py
+++ price_scraper/apps/price_lookup/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
 
-class SearchDetails(models.Model):
-    search_url = models.URLField()
-    product_card = models.CharField(max_length=50)
+class StoreSearchDetails(models.Model):
     title_class = models.CharField(max_length=50)
     price_class = models.CharField(max_length=50)
     available_class = models.CharField(max_length=50)
 
-    product = models.ForeignKey("products.Product", related_name="product_details", on_delete=models.CASCADE)
+    store = models.ForeignKey("stores.Store", related_name="store_search", on_delete=models.CASCADE)
+
+
+class ProductSearchDetails(models.Model):
+    search_url = models.URLField()
+
+    product = models.ForeignKey("products.Product", related_name="product_search", on_delete=models.CASCADE)
     store = models.ForeignKey("stores.Store", related_name="search_details", on_delete=models.CASCADE)
 
     def __str__(self):
