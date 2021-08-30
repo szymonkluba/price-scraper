6c88e2a371301b5539e805152736ad7e2038640a
Szymon Kluba <szymonkluba@gmail.com>
2021-08-30 16:01:18 +0200

    Adds additional fields in serializers.

--- price_scraper/apps/products/serializers.py
+++ price_scraper/apps/products/serializers.py
@@ -5,16 +5,21 @@ from ..price_lookup.serializers import PriceSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Product
         fields = ["name", "popularity", "category", "current_prices"]
 
-        current_prices = PriceSerializer(many=True, read_only=True)
+    current_prices = serializers.SerializerMethodField()
+
+    def get_current_prices(self, obj):
+        prices = obj.product_prices.order_by('timestamp').distinct('store')
+        serializer = PriceSerializer(prices, many=True)
+        return serializer.data
 
 
 class CategorySerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Category
-        fields = ["name"]
+        fields = ["name", "products"]
+
+    products = serializers.HyperlinkedIdentityField(view_name="products_list")
