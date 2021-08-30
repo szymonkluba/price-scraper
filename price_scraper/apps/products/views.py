b7c0badb63b949f580b3283a89bbc2e875adc8ec
Szymon Kluba <szymonkluba@gmail.com>
2021-08-30 16:01:59 +0200

    Adds visits counting for products.

--- price_scraper/apps/products/views.py
+++ price_scraper/apps/products/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django.db.models import F
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
@@ -14,3 +16,10 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.update(popularity=F("popularity") + 1)
+        serializer = self.get_serializer(instance)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
