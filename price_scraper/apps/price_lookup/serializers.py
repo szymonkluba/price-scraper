82e8c934292e82f6e9b52ed6f545c032c4b71cbb
Szymon Kluba <szymonkluba@gmail.com>
2021-08-30 16:02:40 +0200

    Adds additional fields to serializer.

--- price_scraper/apps/price_lookup/serializers.py
+++ price_scraper/apps/price_lookup/serializers.py
@@ -4,5 +4,8 @@ from .models import Price
 
 
 class PriceSerializer(serializers.ModelSerializer):
-    model = Price
-    fields = ['price', 'available', 'timestamp']
+    store = serializers.SlugRelatedField(read_only=True, slug_field='name')
+
+    class Meta:
+        model = Price
+        fields = ['price', 'store', 'available', 'timestamp']
