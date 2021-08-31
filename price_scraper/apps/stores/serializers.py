af58e71b7011ef71893c91cde71fff569d23999a
Szymon Kluba <szymonkluba@gmail.com>
2021-08-31 22:37:32 +0200

    Adds view for store products and prices.

--- price_scraper/apps/stores/serializers.py
+++ price_scraper/apps/stores/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse_lazy
 
 from .models import Store
 
 
 class StoreSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Store
         fields = ["name", "url", "store_prices"]
 
+    store_prices = serializers.SerializerMethodField()
 
+    def get_store_prices(self, obj):
+        return reverse_lazy("store-products", kwargs={"slug": obj.slug})
