4db99739d26149064ec79f72f81e1b7615f9ee76
Szymon Kluba <szymonkluba@gmail.com>
2021-09-01 23:40:14 +0200

    Adds QOL changes in models:D

--- price_scraper/apps/price_lookup/models.py
+++ price_scraper/apps/price_lookup/models.py
@@ -2,17 +2,23 @@ from django.db import models
 
 
 class StoreSearchDetails(models.Model):
+    class Meta:
+        verbose_name_plural = "Stores Search Details"
+
     title_class = models.CharField(max_length=50)
     price_class = models.CharField(max_length=50)
     available_class = models.CharField(max_length=50)
 
     store = models.OneToOneField("stores.Store", related_name="search_details", on_delete=models.CASCADE, unique=True)
 
+    def __str__(self):
+        return f"Selectors: {self.store.name}"
+
 
 class ProductSearchDetails(models.Model):
-
     class Meta:
         unique_together = ["store", "product"]
+        verbose_name_plural = "Product Search Details"
 
     search_url = models.URLField()
 
@@ -31,3 +37,6 @@ class Price(models.Model):
     product = models.ForeignKey("products.Product", related_name="product_prices", on_delete=models.CASCADE)
     store = models.ForeignKey("stores.Store", related_name="store_prices", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.store.name} - {self.timestamp}: " \
+               f"{self.product.name[:25] + '...' if len(self.product.name) > 25 else self.product.name} - {self.price}"
