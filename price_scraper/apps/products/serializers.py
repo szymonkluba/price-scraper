5d81f330f6d0168eb09b3474b3843324a253c839
Szymon Kluba <szymonkluba@gmail.com>
2021-09-01 23:25:27 +0200

    Adds additional field for product url in serializer.

--- price_scraper/apps/products/serializers.py
+++ price_scraper/apps/products/serializers.py
@@ -8,20 +8,24 @@ from ..price_lookup.serializers import PriceSerializer
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["name", "popularity", "category", "current_prices"]
+        fields = ["name", "slug", "url", "popularity", "category", "current_prices"]
 
     current_prices = serializers.SerializerMethodField()
+    url = serializers.SerializerMethodField()
 
     def get_current_prices(self, obj):
         prices = obj.product_prices.order_by("store", "-timestamp").distinct("store")
         serializer = PriceSerializer(prices, many=True)
         return serializer.data
 
+    def get_url(self, obj):
+        return reverse_lazy("product-detail", kwargs={"slug": obj.slug})
+
 
 class CategoryDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ["name", "products"]
+        fields = ["name", "slug", "products"]
 
     products = serializers.SerializerMethodField()
 
@@ -32,7 +36,7 @@ class CategoryDetailSerializer(serializers.ModelSerializer):
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ["name", "url"]
+        fields = ["name", "slug", "url"]
 
     url = serializers.SerializerMethodField()
 
