e9dcec72dfd7a6e2775ceb873d19ac7320aa49ca
Szymon Kluba <szymonkluba@gmail.com>
2021-09-10 23:27:40 +0200

    Final adjustments of product tag.

--- ui/price_scraper/src/components/priceTag.js
+++ ui/price_scraper/src/components/priceTag.js
@@ -9,7 +9,11 @@ class PriceTag extends React.Component {
             <div className={'price-tag'}>
                 <span className={'price-tag-store'}>{this.props.price.store}</span>
                 <span className={'price-tag-price'}>{this.props.price.price} PLN</span>
-                <span className={'available'}>{this.props.price.available ? <FontAwesomeIcon icon={faCheckCircle} size={'lg'}/> : <FontAwesomeIcon icon={faTimesCircle} />}</span>
+                <span className={'available'}>{this.props.price.available ?
+                    <FontAwesomeIcon icon={faCheckCircle}
+                                     size={'lg'}/> :
+                    <FontAwesomeIcon icon={faTimesCircle}
+                                     size={'lg'}/>}</span>
                 <span className={'price-tag-timestamp'}>{moment(this.props.price.timestamp, moment.ISO_8601).fromNow()}</span>
             </div>
         );
